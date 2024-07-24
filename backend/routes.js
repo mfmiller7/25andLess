63f5341c19d8c3e6e73743dfe7ae2d9fba818768
Margo Miller <123111740+mfmiller7@users.noreply.github.com>
2024-07-24 18:35:04 -0400

    Delete backend directory

--- backend/routes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const { getEvents } = require('./controllers/eventsController');
-
-const router = express.Router();
-
-router.get('/:venueID', getEvents);
-
-module.exports = router;

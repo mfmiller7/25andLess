ce44ca47d6ff265427cff9e27fee9291b8aefe6f
Margo Miller <123111740+mfmiller7@users.noreply.github.com>
2024-08-13 18:23:45 -0600

    Update geocodeAddress.ts

--- frontend/src/geocodeAddress.ts
+++ frontend/src/geocodeAddress.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_KEY = 'AIzaSyAZJsWB997890tlhuYgk4uDhoVKiZAsQuQ'; // process.env.REACT_APP_GOOGLE_MAPS_API_KEY;
+const API_KEY = process.env.REACT_APP_GOOGLE_MAPS_API_KEY;
 
 export const geocodeAddress = async (address: any) => {
   try {
